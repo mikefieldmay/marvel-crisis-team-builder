caacc80375c1a655ff0de99cd9e80c15063e825f
Mike Field-May <mikefieldmay@mikes-mbp.lan>
2020-06-11 17:53:56 +0100

    fix: add mobile styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,33 @@ import React, { Component } from "react";
 
 import { CharacterList } from "./components/CharacterList/CharacterList";
 import { Header } from "./components/Header/Header";
-import { CrisisProvider } from "./context/CrisisTeamContext";
+import { CrisisProvider, useCrisisState } from "./context/CrisisTeamContext";
 import { Footer } from "./components/Footer/Footer";
+import { ImageModal } from "./components/ImageModal/ImageModal";
+
 import * as styles from "./index.css";
+
+const AppSkeleton = () => {
+  const {
+    state: { selectedCharacter }
+  } = useCrisisState();
+  return (
+    <>
+      <Header />
+      <CharacterList />
+      <Footer />
+      {selectedCharacter && <ImageModal />}
+    </>
+  );
+};
 
 export class App extends Component {
   render() {
     return (
       <div className={styles.Background}>
-        <Header />
         <CrisisProvider>
-          <CharacterList />
+          <AppSkeleton />
         </CrisisProvider>
-        <Footer />
       </div>
     );
   }
---
+++ src/components/ImageModal/ImageModal.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+
+import * as styles from "./styles.css";
+import { useCrisisState } from "../../context/CrisisTeamContext";
+import { Loading } from "../Icons/Loading";
+import { Flip } from "../Icons/Flip";
+import { setSelectedCharacter } from "../../context/actions";
+
+export const ImageModal = () => {
+  const {
+    state: { selectedCharacter },
+    dispatch
+  } = useCrisisState();
+  const [imageLoading, setImageLoading] = useState(true);
+  const [injuredSide, setInjuredSide] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    setImageLoading(true);
+    setInjuredSide(false);
+    return () => (document.body.style.overflow = "initial");
+  }, [selectedCharacter]);
+
+  if (!selectedCharacter) return null;
+
+  return (
+    <div className={styles.ImageContainer}>
+      <button
+        className={styles.CloseButton}
+        onClick={() => dispatch(setSelectedCharacter(null))}
+      >
+        Close
+      </button>
+      {imageLoading && (
+        <div className={styles.LoadingContainer}>
+          <Loading />
+        </div>
+      )}
+      <div className={styles.ImageWrapper}>
+        {selectedCharacter.name !== "Hulk" && (
+          <button
+            title={`Flip to ${injuredSide ? "Healthy" : "Injured"}`}
+            onClick={() => {
+              setInjuredSide(!injuredSide);
+              setImageLoading(true);
+            }}
+            className={styles.IconContainer}
+          >
+            <Flip />
+          </button>
+        )}
+        <img
+          onLoad={() => {
+            setImageLoading(false);
+          }}
+          className={styles.Image}
+          src={
+            selectedCharacter.images[injuredSide ? "injured" : "healthy"].src
+          }
+          alt={
+            selectedCharacter.images[injuredSide ? "injured" : "healthy"].alt
+          }
+        />
+      </div>
+    </div>
+  );
+};
